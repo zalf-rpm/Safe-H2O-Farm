a90bf6e53677085fa33d92b26c1aeba358b306d0
Xin <isyang_xin@163.com>
2026-02-25 22:13:18 +0100

    output soc-sum3

--- run-consumer-grid.py
+++ run-consumer-grid.py
@@ -83,8 +83,8 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         # "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "NLeach": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "SumNUp": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
-        "Irrig": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
-        # "SOC-sum3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
+        # "Irrig": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
+        "SOC-sum3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "DOY-3": {"data": make_dict_nparr(), "cast-to": "int", "digits": 0},
 
     }
