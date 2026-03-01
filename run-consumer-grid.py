0ac60a6f0b77bd20e4115692d2bed0d120d34463
Xin <isyang_xin@163.com>
2026-03-01 21:13:21 +0100

    output yield

--- run-consumer-grid.py
+++ run-consumer-grid.py
@@ -81,7 +81,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
 
     output_grids = {
         "AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
-        # "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
+        "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "NLeach": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "SumNUp": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "Irrig": {"data": make_dict_nparr(), "cast-to": "int", "digits": 0},
