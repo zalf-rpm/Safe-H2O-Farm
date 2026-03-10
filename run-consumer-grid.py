7662fdb6549f223b8ed4c42391a122285c0af553
Xin <isyang_xin@163.com>
2026-03-10 15:55:56 +0100

    output AtmCO2

--- run-consumer-grid.py
+++ run-consumer-grid.py
@@ -81,7 +81,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
 
     output_grids = {
         # "AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
-        "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
+        # "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "NLeach": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "SumNUp": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "Irrig": {"data": make_dict_nparr(), "cast-to": "int", "digits": 0},
@@ -94,6 +94,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         # "Tavg": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "Tmax": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "Evapotranspiration": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
+        "AtmCO2": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
 
     }
     output_keys = list(output_grids.keys())
