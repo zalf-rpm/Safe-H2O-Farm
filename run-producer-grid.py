c467a9be71e0e1ec7677570c67cd4e9b06a2c44c
Xin <isyang_xin@163.com>
2026-04-14 00:50:14 +0200

    setup["CO2s"]

--- run-producer-grid.py
+++ run-producer-grid.py
@@ -401,8 +401,8 @@ def run_producer(server={"server": None, "port": None}, shared_id=None):
                 # if setup["CO2"]:
                 #     env_template["params"]["userEnvironmentParameters"]["AtmosphericCO2s"] = float(setup["CO2"])
 
-                # if setup["CO2s"]:
-                #     env_template["params"]["userEnvironmentParameters"]["AtmosphericCO2s"] = json.loads(setup["CO2s"])
+                if setup["CO2s"]:
+                    env_template["params"]["userEnvironmentParameters"]["AtmosphericCO2s"] = json.loads(setup["CO2s"])
 
                 # if setup["FieldConditionModifier"]:
                 #     env_template["cropRotation"][0]["worksteps"][0]["crop"]["cropParams"]["species"][
